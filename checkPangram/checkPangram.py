f705491a6a7de776ad05902bdbe7193986733337
sertac1907 <149004154+sertac1907@users.noreply.github.com>
2023-12-15 14:28:18 +0300

    Update checkPangram.py

--- checkPangram/checkPangram.py
+++ checkPangram/checkPangram.py
@@ -1,1 +1,16 @@
 # REJECTED, TRY AGAIN... 14.12.2023
+# Online Python-3 Compiler (Interpretex
+x=input("Metin Girin :")
+x=x.upper()
+z=0
+y=("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+for m in range(26):
+    for n in range(len(x)):
+        if x[n]==y[m]:
+            z=z+1
+            
+
+if z >= 26 :   
+    print("he")
+else : 
+    print("yok")
