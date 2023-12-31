61e574f9c7185c8aba78133fe41afbf1ca1d46c2
sertac1907 <kandemirsertac0@gmail.com>
2023-12-31 17:47:25 +0300

    ticTacToe almost solved

---
+++ ticTacToe/ticTacToe.py
@@ -0,0 +1,16 @@
+player1=input("Enter the first player's name: ")
+player2=input("Enter the second player's name: ")
+
+while True:
+    print("""
+	     |     |
+	     |     |
+	_____|_____|_____
+	     |     |
+	     |     |
+	_____|_____|_____
+	     |     |
+	     |     |
+	     |     |
+    """)
+    move1=input(f"{player1}, where do you want to place your mark ? ")
